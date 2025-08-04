c9f17c7dc787da12dbfc2ae8bee586b7d351e2d5
Aaron <C-Shart@users.noreply.github.com>
2025-08-04 10:38:34 -0700

    removed unneded pragma config

--- ImageHandler.cs
+++ ImageHandler.cs
@@ -1,6 +1,4 @@
-﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
-
-using IronSoftware.Drawing;
+﻿using IronSoftware.Drawing;
 using System.Text;
 using Color = IronSoftware.Drawing.Color;
 using Point = IronSoftware.Drawing.Point;
